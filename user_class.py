749e0390a85b389079a58096e78c1026376fc611
Komal Kaur <komalkaur@berkeley.edu>
2025-08-01 13:08:17 -0700

    start corr and plot

--- user_class.py
+++ user_class.py
@@ -24,6 +24,8 @@ class User:
         None
         '''
 
+        #TODO: implement checking valid data type for symptoms list and data
+
         self.name = name
         self.symptoms = symptoms
         self.data = data
@@ -37,7 +39,13 @@ class User:
         datapath : str
             pathname to the data sheet where the columns are symptom, 
             rows are date, and entries are intensity on a scale from 0 (none) to 5 (severe)
+
+        returns
+        ----------
+        None
         '''
+
+        #TODO: implement warnings for invalid datapaths
 
         current_symptoms = [symp.name for symp in self.symptoms]
         self.data = pd.read_csv(datapath)
@@ -46,3 +54,14 @@ class User:
         for symp in self.data.columns:
             if symp not in current_symptoms:
                 self.symptoms.append(Symptom(symp))
+
+    def corr(self, symp1, symp2):
+        '''
+        find the correlation between two of the user's symptoms 
+        and create a plot of the two together
+        '''
+
+    def plot(self, start=None, end=None):
+        '''
+        plot of all symptoms in time domain
+        '''
